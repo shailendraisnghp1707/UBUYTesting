c52f15f372d73dab842a1c6e0fb121ca524a2218
Shailendra <Shailendra@DESKTOP-OQ31N88>
2019-12-11 16:44:37 +0530

    First Commit

---
+++ src/testing/Add.java
@@ -0,0 +1,17 @@
+package testing;
+
+public class Add {
+	
+	public static void main(String[] args) {
+		
+		
+		int a= 10;
+		int b = 20;
+		
+		int c = a+b;
+		
+		System.out.println("Sum is "+c);
+				
+	}
+
+}
